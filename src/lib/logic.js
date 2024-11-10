db2948ad121a52d318996419f5d9a4be1c052a57
soohyun <s2ksh_77@naver.com>
2024-11-10 16:33:46 +0900

    refactor: 트리노드 mousehover시 fetch하도록 적용 및 함수 리팩터링

--- src/lib/logic.js
+++ src/lib/logic.js
@@ -3,51 +3,79 @@ import { fetchDepartmentNode, fetchUserList } from "./api.js";
 
 export async function initializeTree(rootCode = "0", targetElement) {
   const rootData = await fetchDepartmentNode(rootCode);
-  const rootNode = createTreeNode(rootData[0], 0, "node");
 
-  const rootUl = document.createElement("ul");
-  rootUl.style.padding = "0px 20px";
-  rootUl.appendChild(rootNode);
+  const rootNode = createTreeNode(rootData[0], 0, "node");
+  const rootUl = createRootUl(rootNode);
   targetElement.appendChild(rootUl);
 
-  const rootWrapper = rootNode.querySelector(".tree-node-wrapper");
-  rootWrapper.classList.add("selected");
+  setInitialSelectedNode(rootNode);
 
   toggleEventDelegation(targetElement);
 
-  await fetchChildrenNode(rootNode, rootData[0].code, 1);
+  await fetchChildrenNode({
+    parentNode: rootNode,
+    parentCode: rootData[0].code,
+    depth: 1,
+    type: "html",
+    hidden: false,
+  });
 
   return rootData[0];
 }
 
-export async function fetchChildrenNode(
+function createRootUl(rootNode) {
+  const rootUl = document.createElement("ul");
+  rootUl.style.padding = "0px 20px";
+  rootUl.appendChild(rootNode);
+  return rootUl;
+}
+
+function setInitialSelectedNode(rootNode) {
+  const rootWrapper = rootNode.querySelector(".tree-node-wrapper");
+  rootWrapper.classList.add("selected");
+}
+
+export async function fetchChildrenNode({
   parentNode,
   parentCode,
   depth,
-  type = "html"
-) {
+  type = "html",
+  hidden = true,
+}) {
   const childrenData = await fetchDepartmentNode(parentCode);
   type === "html"
-    ? appendChildListHTML(childrenData, parentNode, depth, type)
-    : appendChildListNode(childrenData, parentNode, depth, type);
+    ? appendChildListHTML({
+        childrenData,
+        parentNode,
+        depth,
+        hidden,
+      })
+    : appendChildListNode({
+        childrenData,
+        parentNode,
+        depth,
+        hidden,
+      });
 }
 
-function appendChildListHTML(childrenData, parentNode, depth, type) {
+function appendChildListHTML({ childrenData, parentNode, depth, hidden }) {
   const childrenHTML = childrenData
-    .map((child) => createTreeNode(child, depth, type))
+    .map((child) => createTreeNode(child, depth, "html"))
     .join("");
 
   const childrenUl = document.createElement("ul");
+  if (hidden) childrenUl.classList.add("hidden");
   childrenUl.innerHTML = childrenHTML;
 
   parentNode.appendChild(childrenUl);
 }
 
-function appendChildListNode(childrenData, parentNode, depth, type) {
+function appendChildListNode({ childrenData, parentNode, depth, hidden }) {
   const childrenUl = document.createElement("ul");
+  if (hidden) childrenUl.classList.add("hidden");
 
   childrenData.forEach((child) => {
-    const childNode = createTreeNode(child, depth, type);
+    const childNode = createTreeNode(child, depth, "node");
     childrenUl.appendChild(childNode);
   });
 
@@ -81,7 +109,7 @@ function setupTreeNode(department, depth) {
   const wrapper = document.createElement("div");
   const span = document.createElement("span");
   const img = document.createElement("img");
-  const toggleBtn = createToggleButton();
+  const toggleBtn = createToggleButton(depth);
 
   img.src = getTreeImgByDepth(depth);
   li.classList.add("tree-node");
@@ -94,69 +122,101 @@ function setupTreeNode(department, depth) {
 
   span.textContent = department.name;
   li.dataset.code = department.code;
-  li.dataset.depth = department.depth;
+  li.dataset.depth = depth;
   wrapper.style.setProperty("--depth", depth);
   li.appendChild(wrapper);
   return li;
 }
 
-function createToggleButton() {
+function createToggleButton(depth) {
   const button = document.createElement("button");
-  button.textContent = "+";
+  button.textContent = depth === 0 ? "-" : "+";
   button.classList.add("toggle-btn");
   return button;
 }
 
+function toggleSelect(targetElement, wrapper) {
+  const selectedElement = targetElement.querySelector(".selected");
+  if (selectedElement) selectedElement.classList.remove("selected");
+  wrapper.classList.toggle("selected");
+}
+
 function toggleEventDelegation(targetElement) {
-  const handleEvent = async (event) => {
-    const { target } = event;
-    const li = target.closest("li");
+  targetElement.addEventListener("click", (e) =>
+    handleClickEvent(e, targetElement)
+  );
+  targetElement.addEventListener("mouseover", handleMouseOverEvent);
+}
 
-    const handleToggleButton = async (li, button, code, depth) => {
-      const childrenUl = li.querySelector("ul");
-      if (!childrenUl) {
-        await fetchChildrenNode(li, code, Number(depth) + 1);
-        button.textContent = "-";
-      } else {
-        childrenUl.classList.toggle("hidden");
-        button.textContent = childrenUl.classList.contains("hidden")
-          ? "+"
-          : "-";
-      }
-    };
+async function handleClickEvent(event, targetElement) {
+  const { target } = event;
+  const li = target.closest("li");
 
-    const toggleSelect = (wrapper) => {
-      const selectedElement = targetElement.querySelector(".selected");
-      if (selectedElement) selectedElement.classList.remove("selected");
-      wrapper.classList.toggle("selected");
-    };
+  if (!li) return;
 
-    if (target.classList.contains("toggle-btn")) {
-      const button = target.closest("button");
-      const { code, depth } = li.dataset;
-      await handleToggleButton(li, button, code, depth);
-    }
-
-    if (
-      target.classList.contains("tree-node-wrapper") ||
-      target.classList.contains("tree-title")
-    ) {
-      toggleSelect(target.closest("li div"));
-      const { code } = li.dataset;
-      const textContent = li.querySelector("span").textContent;
-      const customEvent = new CustomEvent("nodeClick", {
-        detail: { textContent, code },
+  const handleToggleButton = async (li, button, code, depth) => {
+    const childrenUl = li.querySelector("ul");
+    if (!childrenUl) {
+      await fetchChildrenNode({
+        parentNode: li,
+        parentCode: code,
+        depth: Number(depth) + 1,
+        type: "html",
+        hidden: false,
       });
-      targetElement.dispatchEvent(customEvent);
+      button.textContent = "-";
+    } else {
+      childrenUl.classList.toggle("hidden");
+      button.textContent = childrenUl.classList.contains("hidden") ? "+" : "-";
     }
   };
 
-  targetElement.addEventListener("click", handleEvent);
+  const toggleSelect = (wrapper) => {
+    const selectedElement = targetElement.querySelector(".selected");
+    if (selectedElement) selectedElement.classList.remove("selected");
+    wrapper.classList.toggle("selected");
+  };
+
+  if (target.classList.contains("toggle-btn")) {
+    const button = target.closest("button");
+    const { code, depth } = li.dataset;
+    await handleToggleButton(li, button, code, depth);
+  }
+  if (
+    target.classList.contains("tree-node-wrapper") ||
+    target.classList.contains("tree-title")
+  ) {
+    toggleSelect(target.closest("li div"));
+    const { code } = li.dataset;
+    const textContent = li.querySelector(".tree-title").textContent;
+    const customEvent = new CustomEvent("nodeClick", {
+      detail: { textContent, code },
+    });
+    targetElement.dispatchEvent(customEvent);
+  }
+}
+
+async function handleMouseOverEvent(event) {
+  const { target } = event;
+  const li = target.closest("li");
+
+  if (li && target.classList.contains("tree-node-wrapper")) {
+    const { code, depth } = li.dataset;
+    if (!li.dataset.fetched && code) {
+      li.dataset.fetched = true;
+      await fetchChildrenNode({
+        parentNode: li,
+        parentCode: code,
+        depth: Number(depth) + 1,
+        type: "html",
+        hidden: true,
+      });
+    }
+  }
 }
 
 export async function fetchUsersByDepartment(departmentCode) {
   const userList = await fetchUserList();
-  /** departmentCodePath 에 포함되어있는거 다 줘야하나 */
   return userList.filter((user) => user.departmentCode === departmentCode);
 }
 
